f80b78aac6056bbbfecf060b1815058998005edf
NurjanTim <nurjan.huseynova@timgroup.az>
2025-01-17 21:15:48 +0400

    test

--- src/app/confirm-account/[token]/page.tsx
+++ src/app/confirm-account/[token]/page.tsx
@@ -1,12 +1,12 @@
-import ConfirmAccount from '@/pages/ConfirmAccount/ConfirmAccount'
-import React from 'react'
+import ConfirmAccount from "@/pages/ConfirmAccount/ConfirmAccount";
+import React from "react";
 
 function page() {
   return (
     <div>
       <ConfirmAccount />
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
